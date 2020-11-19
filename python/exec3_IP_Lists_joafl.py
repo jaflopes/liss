7f67f0370946c13cd8d6f39512b0e152068bdd2e
Joao Andre Farias Lopes <lopes.jaf@gmail.com>
2020-11-19 22:52:26 +0000

    Renomear ficheiro

---
+++ python/exec3_IP_Lists_joafl.py
@@ -0,0 +1,23 @@
+print("*** List of IPs ***")
+ltIPs = ['127.0.0.1', '192.168.1.1', '192.168.1.2', '192.168.1.3', '255.255.255.0']
+ltIPs_list = [x for x in ltIPs if '192.168.1.' in x]
+print(ltIPs_list)
+b_file = open("exec3_portList.txt", "w")
+print(ltIPs_list, file=b_file)
+b_file.close()
+
+
+print("*** Numeros inteiros ***")
+# Python3 Program to Create list  
+# with integers within given range  
+  
+def createList(r1, r2): 
+    return [item for item in range(r1, r2+1)] 
+      
+# Driver Code 
+r1, r2 = 1, 1024
+listInteger = createList(r1, r2)
+print("see the file")
+b_file = open("exec3_ipList.txt", "w")
+print(listInteger, file=b_file) 
+b_file.close()
